7dfb963e46baf411b17b0cffe26430962a9b115c
erlm <erlm@kea.dk>
2024-02-06 09:57:58 +0100

    Initial commit

---
+++ src/main/java/com/example/githubactionstartv2/GithubActionStartV2Application.java
@@ -0,0 +1,13 @@
+package com.example.githubactionstartv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionStartV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubActionStartV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/githubactionstartv2/GithubActionStartV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubactionstartv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionStartV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
